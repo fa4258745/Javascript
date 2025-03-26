4a03c8dfd16271754833225951802bc879320608
Faraz Ahmed <fa4258745@gmail.com>
2025-03-27 00:08:00 +0530

    commit

--- 20-1-2025-String/homeowrk.js
+++ 20-1-2025-String/homeowrk.js
@@ -4,23 +4,23 @@ function davidGoggins() {
 
   //    <--------CharAt------>
   // Returns character at a specified index
-  // let i ="javascript world";
-  // document.write(i.charAt(5));
+  let i ="javascript world";
+  document.write(i.charAt(5));
 
   //    <--------TRIM------>
   // Remove the trailing white space  between the two index form the string
   // let j="   ReactComponent    ";
   // document.write(j.trim());
 
-     <--------SPLIT------>
-  Split a string into an array
-  let k ="splitting";
-  document.write(k.split(" "))
+  //    <--------SPLIT------>
+  // Split a string into an array
+  // let k ="splitting";
+  // document.write(k.split(" "))
 
-  //    <--------CharCodeAt------>
-  // returns unicode value of character at specified index
-  // let m="ABCcharacterCode";
-  // document.write(m.charCodeAt(0));
+     <--------CharCodeAt------>
+  returns unicode value of character at specified index
+  let m="ABCcharacterCode";
+  document.write(m.charCodeAt(0));
 
   //    <--------indexOf------>
   // finds the first occurence of a string
